423c23ffb7038f470449fd18f43a2e3577d0916d
rad <rmichal@softserveinc.com>
2017-02-01 11:09:22 +0100

    add do test

---
+++ ClassTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ClassTest
+ *
+ * @author rad
+ */
+
+declare(strict_types=1);
+
+class ClassTest {
+    
+    private $name;
+    private $age;
+    private $likeIt;
+    private $cashInYourWallet;
+    
+    public function __construct() {
+        $this->name="Adam";
+        $this->age=22;
+        $this->likeIt=true;
+        $this->cashInYourWallet=3456.46;
+    }
+    
+    public function setName(String $param){
+        $this->name=$param;
+    }
+    public function getName():String {
+        return $this->name;
+    }
+    
+    public function setAge(int $param){
+        $this->age=$param;
+    }
+    public function getAge():int{
+        return $this->age;
+    }
+    public function setLikeIt(bool $param){
+        $this->likeIt=$param;
+    }
+    public function getLikeIt(): bool{
+        return $this->likeIt;
+    }
+    public function setInWollet(float $param){
+        $this->cashInYourWallet=$param;
+    }
+    public function getInWollet():float{
+        return $this->cashInYourWallet;
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-include 'views/findCreateUser.php';
-?>
+
+//declare (strict_types=1);// najwazniejsza deklaracja
+
+
+function sumOld($a,$b){
+    return $a+$b;
+}
+
+function sumNew(int $a, int $b): int {
+    return $a+$b;
+}
+
+echo sumOld("10 abc",5)." 1 <br>";
+echo sumNew("10 abc",5)." 2 <br>";//error
+
+echo sumNew(10,5)." 3 <br>";
+echo sumNew("10",5)." 4 <br>";//error
+echo sumOld("10",5)." 5 <br>";
